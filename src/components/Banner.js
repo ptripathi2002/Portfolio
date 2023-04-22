25e817578a8d12e3830621b54396733dfeb0f246
Prashant Tripathi <90204816+ptripathi2002@users.noreply.github.com>
2023-04-23 00:43:01 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -53,7 +53,7 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Prashant, a `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Competitive Programmer", "Tech Explorer" ]'><span className="wrap">{text}</span></span></h1>
+                <h3>{`Hi! I'm Prashant, a `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Competitive Programmer", "Tech Explorer" ]'><span className="wrap">{text}</span></span></h3>
                   <p>Currently Pursuing my Btech from Ajay Kumar Garg Engineering College, Ghaziabad in Computer Science Branch</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
              
